247c90e8704a744ca838a93e44741bb07e5ccb76
BDoss2003 <bjdoss1@buffs.wtamu.edu>
2024-05-02 21:26:46 -0500

    More Tests

--- projects/Barky2024_Refactor_3/src/djbarky/barkyarch/tests.py
+++ projects/Barky2024_Refactor_3/src/djbarky/barkyarch/tests.py
@@ -32,7 +32,7 @@ class TestCommands(TestCase):
             notes="Test notes 2",
             date_added=right_now,
         )
-
+    #BOOKMARK tests
     def test_command_add(self):
         add_command = AddBookmarkCommand()
         add_command.execute(self.domain_bookmark_1)
@@ -64,3 +64,91 @@ class TestCommands(TestCase):
         with self.assertRaises(Exception):  # Adjust the expected exception based on error handling
             add_command.execute(invalid_bookmark)
         self.assertEqual(Bookmark.objects.count(), 0)  # No bookmark should be added
+
+    #LIST tests
+    
+    def test_command_list_default_order(self):
+        list_command = ListBookmarksCommand()
+        # Add some bookmarks
+        add_command = AddBookmarkCommand()
+        add_command.execute(self.domain_bookmark_1)
+        add_command.execute(self.domain_bookmark_2)
+
+        # Retrieve bookmarks and verify default order (by date_added)
+        bookmarks = list_command.execute()
+        self.assertEqual(len(bookmarks), 2)
+        self.assertEqual(bookmarks[0].url, self.domain_bookmark_1.url)
+        self.assertEqual(bookmarks[1].url, self.domain_bookmark_2.url)
+
+    def test_command_list_custom_order(self):
+        list_command = ListBookmarksCommand(order_by="title")
+        # Add some bookmarks
+        add_command = AddBookmarkCommand()
+        add_command.execute(self.domain_bookmark_1)
+        add_command.execute(self.domain_bookmark_2)
+
+        # Retrieve bookmarks and verify custom order (by title)
+        bookmarks = list_command.execute()
+        self.assertEqual(len(bookmarks), 2)
+        self.assertEqual(bookmarks[0].url, self.domain_bookmark_2.url)
+        self.assertEqual(bookmarks[1].url, self.domain_bookmark_1.url)
+
+    def test_command_list_empty_list(self):
+        list_command = ListBookmarksCommand()
+        # Retrieve bookmarks when the list is empty
+        bookmarks = list_command.execute()
+        self.assertEqual(len(bookmarks), 0)
+    
+    # DELETE Tests
+    def test_command_delete_existing_bookmark(self):
+        add_command = AddBookmarkCommand()
+        add_command.execute(self.domain_bookmark_1)
+        
+        delete_command = DeleteBookmarkCommand()
+        delete_command.execute(self.domain_bookmark_1)
+        
+        # Verify that the bookmark has been deleted
+        self.assertEqual(Bookmark.objects.count(), 0)
+
+    def test_command_delete_nonexistent_bookmark(self):
+        delete_command = DeleteBookmarkCommand()
+        
+        # Attempt to delete a bookmark that does not exist
+        with self.assertRaises(Bookmark.DoesNotExist):
+            delete_command.execute(self.domain_bookmark_2)
+    
+    #Edit Tests
+    def test_command_edit_existing_bookmark(self):
+        add_command = AddBookmarkCommand()
+        add_command.execute(self.domain_bookmark_1)
+        
+        updated_bookmark = DomainBookmark(
+            id=1,
+            title="Updated Bookmark",
+            url="http://www.updated.com",
+            notes="Updated notes",
+            date_added=self.domain_bookmark_1.date_added,
+        )
+        
+        edit_command = EditBookmarkCommand()
+        edit_command.execute(updated_bookmark)
+        
+        # Retrieve and verify the updated bookmark
+        updated_db_bookmark = Bookmark.objects.get(id=1)
+        self.assertEqual(updated_db_bookmark.title, "Updated Bookmark")
+        self.assertEqual(updated_db_bookmark.url, "http://www.updated.com")
+
+    def test_command_edit_nonexistent_bookmark(self):
+        updated_bookmark = DomainBookmark(
+            id=3,
+            title="Updated Bookmark",
+            url="http://www.updated.com",
+            notes="Updated notes",
+            date_added=self.domain_bookmark_1.date_added,
+        )
+        
+        edit_command = EditBookmarkCommand()
+        
+        # Attempt to edit a bookmark that does not exist
+        with self.assertRaises(Bookmark.DoesNotExist):
+            edit_command.execute(updated_bookmark)
